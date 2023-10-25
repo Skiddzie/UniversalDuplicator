9dbf710d3fe7338dfa1e209efef06b5f8c057e4c
Skiddzie <aidanthe1@gmail.com>
2023-10-25 12:06:23 -0400

    cleaned up and moved the zpl code to a different file

--- duplicator.py
+++ duplicator.py
@@ -1,28 +1,13 @@
 import os
+
+from zplTemplate import *
+
 while(True):
     barcode = input(">")
-    template = """
-
-    ^XA
-    ^CF0,60
-    ^F(C100,100,255,255,N,1,0,0,0,0
-    ^FO0,0^FR^GB1000,56,30^FS
-
-    ^CFA,40
-    ^F(C0,0,0,255,Y,100,100,255,255,0
-    ^FO150,5^FDProperty Of My Library^FS
-
-    ^CFA,30
-
-
-    ^BY3,2,150
-    ^FO160,120^BC^FD"""+barcode+"""^FS
-
-    ^XZ
-
-    """
+    template = templateFill(barcode)
 
     print(template)
+
     with open(r'zpl.txt', 'w') as f:
         f.write(template)
     os.startfile(r"zpl.txt", "print")
---
+++ zplTemplate.py
@@ -0,0 +1,23 @@
+#add or remove parameters depending on what the client needs
+def template(field1):
+    return """
+
+    ^XA
+    ^CF0,60
+    ^F(C100,100,255,255,N,1,0,0,0,0
+    ^FO0,0^FR^GB1000,56,30^FS
+
+    ^CFA,40
+    ^F(C0,0,0,255,Y,100,100,255,255,0
+    ^FO150,5^FDProperty Of My Library^FS
+
+    ^CFA,30
+
+
+    ^BY3,2,150
+    ^FO160,120^BC^FD"""+field1+"""^FS
+
+    ^XZ
+
+    """
+
